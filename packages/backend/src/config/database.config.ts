e14fd2f446b7e5d999b14674d666e67b11546cef
minhoyunlife <minhoyun.life@gmail.com>
2024-10-18 18:36:49 +0900

    chore: set autoLoadEntities to database config

--- packages/backend/src/config/database.config.ts
+++ packages/backend/src/config/database.config.ts
@@ -1,10 +1,4 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 /**
  * 데이터베이스 설정
@@ -16,7 +10,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   username: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'password',
   database: process.env.POSTGRES_DB || 'db',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  autoLoadEntities: true,
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV !== 'production',
   ssl: process.env.NODE_ENV === 'production',
