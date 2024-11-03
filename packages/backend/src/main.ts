9746df0bd3ec5dafa19cf783b94912d346c9d471
minhoyunlife <minhoyun.life@gmail.com>
2024-11-03 17:56:06 +0900

    feat: update openapi submodule and set validation pipes

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,20 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@/src/app.module';
+import { validationConfig } from '@/src/config/validation.config';
 
 (async function () {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      validationError: {
-        target: true,
-        value: true,
-      },
-      transform: true, // class-transformer를 이용 가능하도록
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
 
   await app.listen(process.env.APP_PORT || 3000);
 })();
