c48f9e81bc1a2a81195eb5bbc6b95ef55429f0e7
minhoyunlife <minhoyun.life@gmail.com>
2024-11-03 17:56:27 +0900

    feat: implement artworks controller

--- packages/backend/src/modules/artworks/artworks.controller.ts
+++ packages/backend/src/modules/artworks/artworks.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
-import { ArtworksService } from './artworks.service';
+import { ArtworksService } from '@/src/modules/artworks/artworks.service';
+import { ArtworkResponse } from '@/src/modules/artworks/dtos/artwork-response.dto';
+import { CreateArtworkDto } from '@/src/modules/artworks/dtos/create-artwork.dto';
 
 @Controller('artworks')
 export class ArtworksController {
   constructor(private readonly artworksService: ArtworksService) {}
 
-  @Get()
-  getArtworks() {
-    return this.artworksService.getArtworks();
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body() createArtworkDto: CreateArtworkDto,
+  ): Promise<ArtworkResponse> {
+    const artwork = await this.artworksService.createArtwork(createArtworkDto);
+    return new ArtworkResponse(artwork);
   }
 }
