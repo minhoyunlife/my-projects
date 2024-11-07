8b5e837335b931f1883be7b0f22a7571f6e2b063
minhoyunlife <minhoyun.life@gmail.com>
2024-11-07 19:18:02 +0900

    chore(common): create ci (#3)
    * chore: define composite action for detecting which packages are changed
    
    * chore: implement verify-backend composite action
    
    * chore: create ci.yml

---
+++ .github/actions/detect-changes/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+base_branch="$1"
+if [[ -z "$base_branch" ]]; then
+  echo "Base branch argument is required."
+  exit 1
+fi
+
+# 코드 변경사항이 발생한 패키지 이름만을 따로 추출해서 배열화
+function get_package_names_from_code_changes() {
+  git diff --name-only "$base_branch"...HEAD | \
+  grep "^packages/" | \
+  cut -d'/' -f2 | \
+  sort -u | \
+  jq -R -s 'split("\n")[:-1]'
+}
+
+# 의존성 변경사항이 발생한 패키지 이름만을 따로 추출해서 배열화
+function get_package_names_from_deps_changes() {
+  pnpm list --filter="...[$base_branch]" --json | \
+  jq '[.[] | select(.name != "my-projects") | .name | sub("^@my-projects/"; "")]'
+}
+
+# 코드, 의존성 변경사항이 있는 패키지 이름을 하나의 배열로 병합
+function merge() {
+  local code_changes="$1"
+  local lock_changes="$2"
+  echo "$code_changes" "$lock_changes" | jq -s "add | unique"
+}
+
+# 메인 로직
+code_changes=$(get_package_names_from_code_changes)
+lock_changes=$(get_package_names_from_deps_changes)
+all_changes=$(merge "$code_changes" "$lock_changes")
+
+echo "packages=${all_changes}" >> $GITHUB_OUTPUT
