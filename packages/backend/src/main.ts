877bde5731f25fb956eb94974b9cb32711bfea8d
minhoyunlife <minhoyun.life@gmail.com>
2024-11-08 17:24:09 +0900

    test: check 5

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
-(async function () {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-})();
-
-// comment
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+         await app.listen(3000);  // 들여쓰기 깨트림
+}
+bootstrap();
