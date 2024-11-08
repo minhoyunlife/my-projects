64b41604527751b02b2a501997e887960d861bf5
minhoyunlife <minhoyun.life@gmail.com>
2024-11-08 17:31:49 +0900

    test: check 6

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';import { AppModule } from './app.module';
 
-import { AppModule } from './app.module';
-
-(async function () {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-})();
-
-// comment
+async function bootstrap(){
+   const unusedVar = 'test';  // 사용하지 않는 변수
+   const app=await NestFactory.create(AppModule)
+        await app.listen(3000)  // 세미콜론 누락
+}
+bootstrap();
