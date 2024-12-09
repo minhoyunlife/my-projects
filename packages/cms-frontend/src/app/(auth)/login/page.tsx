c5a0a50175c7a989e1a29771effbb842b68ee3d2
minhoyunlife <minhoyun.life@gmail.com>
2024-12-09 18:51:12 +0900

    feat: modify login page

--- packages/cms-frontend/src/app/(auth)/login/page.tsx
+++ packages/cms-frontend/src/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { GitHubLoginButton } from "@/src/components/auth/github-login-button";
-import { LOGIN_ERROR_MESSAGES } from "@/src/constants/messages";
-import { useToast } from "@/src/hooks/use-toast";
-import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
+
+import { useSearchParams } from "next/navigation";
+
+import { GitHubLoginButton } from "@/src/components/auth/github-login-button";
+import { isAuthErrorCode } from "@/src/constants/errors/auth/code";
+import { getErrorMessage } from "@/src/constants/errors/auth/messages";
+import { useToast } from "@/src/hooks/use-toast";
 
 export default function LoginPage() {
   const { toast } = useToast();
@@ -12,32 +15,32 @@ export default function LoginPage() {
   const searchParams = useSearchParams();
   const error = searchParams.get("error");
 
-  function isLoginError(
-    error: string,
-  ): error is keyof typeof LOGIN_ERROR_MESSAGES {
-    return error in LOGIN_ERROR_MESSAGES;
-  }
-
+  // 에러 토스트 표시
   useEffect(() => {
-    if (error && isLoginError(error)) {
-      toast({
-        variant: "destructive",
-        description: LOGIN_ERROR_MESSAGES[error],
-      });
+    if (error && isAuthErrorCode(error.toUpperCase())) {
+      const timer = setTimeout(() => {
+        toast({
+          variant: "destructive",
+          description: getErrorMessage(error),
+        });
+      }, 100);
+
+      return () => clearTimeout(timer);
     }
   }, [error, toast]);
 
   return (
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      role="region"
-      aria-label="login"
-    >
-      <h2 className="text-lg font-semibold text-center mb-4">로그인</h2>
+    <section aria-label="login">
+      <h2 className="text-lg font-semibold text-center mb-4">
+        SNS 계정으로 로그인
+      </h2>
       <GitHubLoginButton />
-      <p className="mt-3 text-sm text-yellow-500 dark:text-yellow-400 text-center">
+      <p
+        className="mt-3 text-sm text-yellow-500 dark:text-yellow-400 text-center"
+        role="alert"
+      >
         관리자 계정으로만 로그인이 가능합니다
       </p>
-    </form>
+    </section>
   );
 }
