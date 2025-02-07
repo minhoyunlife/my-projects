7ce4e7f32bbd154a490f66ac707f3c437597ef2a
minhoyunlife <minhoyun.life@gmail.com>
2025-02-07 15:14:51 +0900

    feat: enrich current health check features

--- packages/backend/src/modules/health/health.controller.ts
+++ packages/backend/src/modules/health/health.controller.ts
@@ -1,20 +1,61 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import {
   HealthCheckService,
   HealthCheck,
   TypeOrmHealthIndicator,
+  MemoryHealthIndicator,
+  DiskHealthIndicator,
 } from '@nestjs/terminus';
+
+import { GithubHealthIndicator } from '@/src/modules/health/indicators/github.health.indicator';
+import { S3HealthIndicator } from '@/src/modules/health/indicators/s3.health.indicator';
 
 @Controller('health')
 export class HealthController {
   constructor(
-    private health: HealthCheckService,
-    private db: TypeOrmHealthIndicator,
+    private readonly configService: ConfigService,
+    private readonly health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+    private readonly disk: DiskHealthIndicator,
+    private readonly s3: S3HealthIndicator,
+    private readonly github: GithubHealthIndicator,
   ) {}
 
-  @Get()
+  @Get('/liveness')
   @HealthCheck()
   check() {
-    return this.health.check([() => this.db.pingCheck('database')]);
+    const config = this.configService.get('health');
+    const heapLimit = config.containerMemory * config.heapMemoryThresholdRatio;
+    const rssLimit = config.containerMemory * config.rssMemoryThresholdRatio;
+
+    return this.health.check([
+      // 메모리 체크
+      () => this.memory.checkHeap('heap_memory', heapLimit),
+      () => this.memory.checkRSS('process_memory', rssLimit),
+
+      // 디스크 체크
+      () =>
+        this.disk.checkStorage('disk_space', {
+          thresholdPercent: config.diskThresholdPercent,
+          path: config.diskPath,
+        }),
+    ]);
+  }
+
+  @Get('/readiness')
+  @HealthCheck()
+  async checkReadiness() {
+    return this.health.check([
+      // DB 연결 체크
+      () => this.db.pingCheck('database'),
+
+      // S3 연결 체크
+      () => this.s3.isHealthy('s3'),
+
+      // Github OAuth 연결 체크
+      () => this.github.isHealthy('github_oauth'),
+    ]);
   }
 }
--- packages/backend/src/modules/health/health.module.ts
+++ packages/backend/src/modules/health/health.module.ts
@@ -1,10 +1,15 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
 import { HealthController } from '@/src/modules/health/health.controller';
+import { GithubHealthIndicator } from '@/src/modules/health/indicators/github.health.indicator';
+import { S3HealthIndicator } from '@/src/modules/health/indicators/s3.health.indicator';
+import { StorageModule } from '@/src/modules/storage/storage.module';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [TerminusModule, HttpModule, StorageModule],
   controllers: [HealthController],
+  providers: [S3HealthIndicator, GithubHealthIndicator],
 })
 export class HealthModule {}
