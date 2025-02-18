1713826f7628f5623b51bc89ca065779e4f64b10
minhoyunlife <minhoyun.life@gmail.com>
2025-02-18 15:25:56 +0900

    fix: set base-ref input to change detection composite action (#75)
    * fix: set base-ref input to change detection composite action
    
    * fix: set argument to composite action in ci and cd workflow

--- .github/actions/detect-changes/script.sh
+++ .github/actions/detect-changes/script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo "GITHUB_BASE_SHA: $GITHUB_BASE_SHA"
-echo "Current HEAD: $(git rev-parse HEAD)"
-
-# 실제 변경된 파일 목록 확인
-echo "Changed files:"
-git diff --name-only $GITHUB_BASE_SHA..HEAD
+base_ref="$1"
+if [[ -z "$base_ref" ]]; then
+  echo "Base ref argument is required."
+  exit 1
+fi
 
 # 코드 변경사항이 발생한 패키지 이름만을 따로 추출해서 배열화
 function get_package_names_from_code_changes() {
-  git diff --name-only $GITHUB_BASE_SHA..HEAD | \
+  git diff --name-only "$base_ref"...HEAD | \
   grep "^packages/" | \
   cut -d'/' -f2 | \
   sort -u | \
@@ -18,7 +17,7 @@ function get_package_names_from_code_changes() {
 
 # 의존성 변경사항이 발생한 패키지 이름만을 따로 추출해서 배열화
 function get_package_names_from_deps_changes() {
-  pnpm list --filter="...[$GITHUB_BASE_SHA]" --json | \
+  pnpm list --filter="...[$base_ref]" --json | \
   jq '[.[] | select(.name != "my-projects") | .name | sub("^@my-projects/"; "")]'
 }
 
@@ -26,7 +25,7 @@ function get_package_names_from_deps_changes() {
 function merge() {
   local code_changes="$1"
   local lock_changes="$2"
-  echo "$code_changes" "$lock_changes" | jq -c -s "add | unique"
+  echo "$code_changes" "$lock_changes" | jq -s "add | unique"
 }
 
 # 메인 로직
@@ -34,5 +33,4 @@ code_changes=$(get_package_names_from_code_changes)
 lock_changes=$(get_package_names_from_deps_changes)
 all_changes=$(merge "$code_changes" "$lock_changes")
 
-echo "Changed packages are: $all_changes"
-echo "packages=$all_changes" >> $GITHUB_OUTPUT
+echo "packages=${all_changes}" >> $GITHUB_OUTPUT
