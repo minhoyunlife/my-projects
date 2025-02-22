be2d1a9c42b573894c4f66d280be7723ffed3fc2
minhoyunlife <minhoyun.life@gmail.com>
2025-02-22 20:12:51 +0900

    chore(cd): dummy fix for triggering build (#84)

--- packages/cms-frontend/next.config.ts
+++ packages/cms-frontend/next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
+// INFO: Next.js 에서는 빌드 시점에 환경변수를 평가함에 주의
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
