a7d964b4711de461ceb589133c5396e8f4e6c911
minhoyunlife <minhoyun.life@gmail.com>
2025-03-16 18:27:16 +0900

    fix: fix problem when share through SNS

--- packages/fanarts-frontend/src/routes/+layout.svelte
+++ packages/fanarts-frontend/src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script>
   import '../app.css';
-  import { languageState } from '$lib/states/language.svelte';
 
   let { children } = $props();
-
-  $effect(() => {
-    if (typeof document !== 'undefined') {
-      document.documentElement.lang = languageState.currentLanguage;
-    }
-  });
 </script>
 
 {@render children()}
