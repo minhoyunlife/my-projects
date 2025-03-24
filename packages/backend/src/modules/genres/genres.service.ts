43e571be3b72217aa37d2e3f44be994647ea2601
minhoyunlife <minhoyun.life@gmail.com>
2025-03-24 14:40:07 +0900

    fix(backend): refactor genre modules following five lines of code rules (#106)

--- packages/backend/src/modules/genres/genres.service.ts
+++ packages/backend/src/modules/genres/genres.service.ts
@@ -24,67 +24,77 @@ export class GenresService {
     private readonly entityManager: EntityManager,
   ) {}
 
-  /**
-   * 장르 목록을 쿼리의 조건에 맞춰서 조회
-   * @param query 장르 목록을 조회하기 위한 쿼리 정보
-   * @param query.page 페이지 번호 (기본값: 1)
-   * @param query.search 장르명 검색어
-   * @returns 장르 목록과 총 장르 수
-   */
   async getGenres(query: GetGenresQueryDto): Promise<EntityList<Genre>> {
     const page = query.page ?? 1;
     const search = query.search ?? undefined;
-
     const [items, totalCount] = await this.genresRepository.getAllWithFilters({
       page,
       pageSize: PAGE_SIZE.CMS,
       search,
     });
-
     return {
       items,
       totalCount,
     };
   }
 
-  /**
-   * 장르를 쿼리의 검색어에 맞춰서 검색
-   * @param query 장르 목록을 조회하기 위한 쿼리 정보
-   * @param query.search 장르명 검색어
-   * @returns 장르 목록
-   */
   async getGenresByName(query: GetGenresByNameQueryDto): Promise<Genre[]> {
     return this.genresRepository.findByName(query.search);
   }
 
-  /**
-   * 새로운 장르를 생성
-   * @param dto - 장르 생성 정보를 담은 DTO
-   * @returns 생성된 장르
-   */
   async createGenre(dto: CreateGenreDto): Promise<Genre> {
     return this.entityManager.transaction(async (manager) => {
       const genresTxRepo = this.genresRepository.forTransaction(manager);
-
-      const genreData: Partial<Genre> = {
-        translations: [
-          { language: Language.KO, name: dto.koName },
-          { language: Language.EN, name: dto.enName },
-          { language: Language.JA, name: dto.jaName },
-        ] as GenreTranslation[],
-      };
-
+      const genreData = this.createGenreDataFromDto(dto, null);
       return await genresTxRepo.createOne(genreData);
     });
   }
 
-  /**
-   * 기존 장르를 수정
-   * @param id - 수정할 장르의 ID
-   * @param dto - 장르명의 수정 정보를 담은 DTO
-   * @returns 수정된 장르
-   */
   async updateGenre(id: string, dto: UpdateGenreDto): Promise<Genre> {
+    this.assertAllTranslationNamesExist(dto);
+
+    return this.entityManager.transaction(async (manager) => {
+      const genresTxRepo = this.genresRepository.forTransaction(manager);
+      const genreData = this.createGenreDataFromDto(dto, id);
+      return await genresTxRepo.updateOne(genreData);
+    });
+  }
+
+  async deleteGenres(ids: string[]): Promise<void> {
+    return this.entityManager.transaction(async (manager) => {
+      const genresTxRepo = this.genresRepository.forTransaction(manager);
+      await genresTxRepo.deleteMany(ids);
+    });
+  }
+
+  private createGenreDataFromDto(
+    dto: UpdateGenreDto,
+    id?: string,
+  ): Partial<Genre> {
+    return {
+      id: id ? id : undefined,
+      translations: this.createTranslationsFromDto(dto),
+    };
+  }
+
+  private createTranslationsFromDto(dto: UpdateGenreDto): GenreTranslation[] {
+    const translations = [];
+    this.addTranslationIfProvided(translations, Language.KO, dto.koName);
+    this.addTranslationIfProvided(translations, Language.EN, dto.enName);
+    this.addTranslationIfProvided(translations, Language.JA, dto.jaName);
+    return translations;
+  }
+
+  private addTranslationIfProvided(
+    translations: Partial<GenreTranslation>[],
+    language: Language,
+    name: string,
+  ): void {
+    if (!name) return;
+    translations.push({ language, name });
+  }
+
+  private assertAllTranslationNamesExist(dto: UpdateGenreDto) {
     if (!dto.koName && !dto.enName && !dto.jaName) {
       throw new GenreException(
         GenreErrorCode.NO_TRANSLATIONS_PROVIDED,
@@ -96,38 +106,5 @@ export class GenresService {
         },
       );
     }
-
-    return this.entityManager.transaction(async (manager) => {
-      const genresTxRepo = this.genresRepository.forTransaction(manager);
-
-      const translations = [];
-      if (dto.koName) {
-        translations.push({ language: Language.KO, name: dto.koName });
-      }
-      if (dto.enName) {
-        translations.push({ language: Language.EN, name: dto.enName });
-      }
-      if (dto.jaName) {
-        translations.push({ language: Language.JA, name: dto.jaName });
-      }
-
-      const genreData: Partial<Genre> = {
-        id,
-        translations,
-      };
-
-      return await genresTxRepo.updateOne(genreData);
-    });
-  }
-
-  /**
-   * 복수의 기존 장르를 삭제
-   * @param id - 삭제할 장르의 ID
-   */
-  async deleteGenres(ids: string[]): Promise<void> {
-    return this.entityManager.transaction(async (manager) => {
-      const genresTxRepo = this.genresRepository.forTransaction(manager);
-      await genresTxRepo.deleteMany(ids);
-    });
   }
 }
