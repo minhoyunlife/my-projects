ffd71bcb653d486654c7bbda95cbd0d6003920ca
minhoyunlife <minhoyun.life@gmail.com>
2025-04-02 10:55:51 +0900

    feat: create dtos

---
+++ packages/backend/src/modules/series/dtos/delete-series.dto.ts
@@ -0,0 +1,14 @@
+import { Transform } from 'class-transformer';
+
+import { ValidatedStringArray } from '@/src/common/decorators/validated-array.decorator';
+
+export class DeleteSeriesDto {
+  @ValidatedStringArray({ notEmpty: true })
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return [value].filter(Boolean);
+    }
+    return Array.isArray(value) ? value.filter(Boolean) : value;
+  })
+  ids: string[];
+}
